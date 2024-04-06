dbbee6e9ac131f8649a21de79a0c009678e9932a
rohitbarhe0002 <rohitbarche99@gmail.com>
2024-04-06 14:31:29 +0530

    checked

--- UberMobApp/utils/index.js
+++ UberMobApp/utils/index.js
@@ -1,13 +1,11 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
-
-export  default  getUserToken = async () => {
-    try {
-      userToken = await AsyncStorage.getItem('@userToken_Key');
-      return userToken;
-    } catch (error) {
-      console.error('Error retrieving user token from AsyncStorage:', error);
-    }
+export default getUserToken = async () => {
+  try {
+    userToken = await AsyncStorage.getItem('@userToken_Key');
+    console.log('checked');
+    return userToken;
+  } catch (error) {
+    console.error('Error retrieving user token from AsyncStorage:', error);
   }
-  
- 
+};
